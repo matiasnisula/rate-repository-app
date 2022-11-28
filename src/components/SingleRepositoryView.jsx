54e14ad5f6cdb65dbf874c834d842c4efbbe2df9
Matias Nisula <matiasnisula81@gmail.com>
2022-11-28 18:27:22 +0200

    10.20, uninstall jest causing dependency issues

--- src/components/SingleRepositoryView.jsx
+++ src/components/SingleRepositoryView.jsx
@@ -1,13 +1,123 @@
 import { useParams } from "react-router-native";
+import { FlatList, StyleSheet, View } from "react-native";
+import format from "date-fns/format";
+import Text from "./Text";
+import theme from "../theme";
 import RepositoryItem from "./RepositoryItem";
 import useRepository from "../hooks/useRepository";
+
+const itemStyles = StyleSheet.create({
+  container: {
+    display: "flex",
+    alignItems: "stretch",
+    flexWrap: "wrap",
+    flexGrow: 0,
+    backgroundColor: theme.colors.repositoryItem,
+    marginTop: 5,
+    padding: 10,
+  },
+});
+
+const itemHeaderStyles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    flexGrow: 1,
+  },
+  ratingContainer: {
+    width: 40,
+    height: 40,
+    flexGrow: 0,
+    padding: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 40 / 2,
+    borderWidth: 2,
+    borderColor: theme.colors.primary,
+  },
+  infoContainer: {
+    flexGrow: 1,
+    flexShrink: 1,
+    padding: 5,
+  },
+});
+
+const ItemHeader = ({ username, createdAt, rating }) => {
+  return (
+    <View style={itemHeaderStyles.container}>
+      <View style={itemHeaderStyles.ratingContainer}>
+        <Text fontWeight="bold" style={{ color: theme.colors.primary }}>
+          {rating}
+        </Text>
+      </View>
+      <View style={itemHeaderStyles.infoContainer}>
+        <Text fontWeight="bold" fontSize="subheading">
+          {username}
+        </Text>
+        <Text color="textSecondary">{createdAt}</Text>
+      </View>
+    </View>
+  );
+};
+
+const itemTextStyle = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    flexShrink: 1,
+    paddingVertical: 10,
+  },
+});
+
+const ItemBody = ({ text }) => {
+  return (
+    <View style={itemTextStyle.container}>
+      <Text>{text}</Text>
+    </View>
+  );
+};
+
+const ReviewItem = ({ review }) => {
+  return (
+    <View style={itemStyles.container}>
+      <ItemHeader
+        username={review.user.username}
+        createdAt={review.createdAt}
+        rating={review.rating}
+      />
+      <ItemBody text={review.text} />
+    </View>
+  );
+};
 
 const SingleRepositoryView = () => {
   const { repositoryID } = useParams();
 
   const { repository } = useRepository(repositoryID);
+  const repositoryReviews = repository
+    ? repository.reviews.edges.map((edge) => {
+        return {
+          ...edge.node,
+          createdAt: format(new Date(edge.node.createdAt), "dd.MM.yyyy"),
+        };
+      })
+    : [];
+  
   console.log("repository:", repository);
-  return <RepositoryItem {...repository} showAll={true} />;
+  console.log("reviews:", repositoryReviews);
+  
+  const renderItem = ({ item }) => {
+    return <ReviewItem review={item} />;
+  };
+
+  return (
+    <FlatList
+      data={repositoryReviews}
+      renderItem={renderItem}
+      keyExtractor={({ id }) => id}
+      ListHeaderComponent={() => (
+        <RepositoryItem {...repository} showAll={true} />
+      )}
+    />
+  );
 };
 
 export default SingleRepositoryView;
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -34,6 +34,20 @@ export const GET_REPOSITORY_BY_ID = gql`
   query getRepositoryById($id: ID!) {
     repository(id: $id) {
       ...RepositoryFields
+      reviews {
+        edges {
+          node {
+            id
+            text
+            rating
+            createdAt
+            user {
+              id
+              username
+            }
+          }
+        }
+      }
     }
   }
 `;
